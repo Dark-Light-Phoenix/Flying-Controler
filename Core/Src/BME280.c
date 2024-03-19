024dc99f2f85841c7f03d09896ce17f1d051a0d0
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-03-19 22:19:46 +0200

    Added handler for checking I2C_Mem_Read

--- Core/Src/BME280.c
+++ Core/Src/BME280.c
@@ -2,18 +2,29 @@
 #include "Functions.h"
 #include "math.h"
 #include "stm32f4xx_hal.h"
+#include "stdio.h"
 
 extern I2C_HandleTypeDef hi2c2;
 
 void BME280_First_Scan (double *start_pressure, double *start_temperature)
 {
+	HAL_StatusTypeDef checkP;
+	HAL_StatusTypeDef checkT;
 	uint32_t pressure0;
 	uint8_t PRES_data [3];
 	uint8_t TEM_data [3];
 
-	HAL_I2C_Mem_Read (&hi2c2, BME280_ADDRESS << 1, PRESS_MSB_REG, I2C_MEMADD_SIZE_8BIT, PRES_data, 3, 1000); // Reading pressure data from register
+	checkP = HAL_I2C_Mem_Read (&hi2c2, BME280_ADDRESS << 1, PRESS_MSB_REG, I2C_MEMADD_SIZE_8BIT, PRES_data, 3, 1000); // Reading pressure data from register
+	if (checkP != HAL_OK)
+	{
+ 		printf ("Error");
+	}
 	HAL_Delay (1000);
-	HAL_I2C_Mem_Read (&hi2c2, BME280_ADDRESS << 1, BME280_TEMPERATURE_MSB_REG, I2C_MEMADD_SIZE_8BIT, TEM_data, 3, 1000); // Reading temperature data from register
+	checkT = HAL_I2C_Mem_Read (&hi2c2, BME280_ADDRESS << 1, BME280_TEMPERATURE_MSB_REG, I2C_MEMADD_SIZE_8BIT, TEM_data, 3, 1000); // Reading temperature data from register
+	if (checkT != HAL_OK)
+		{
+			printf ("Error");
+		}
 	HAL_Delay (1000);
 
 	pressure0 = ((uint32_t) PRES_data [0] << 12 | (uint32_t) PRES_data [1] << 4 | (uint32_t) PRES_data [2] >> 4);
@@ -32,18 +43,28 @@ void BME280_First_Scan (double *start_pressure, double *start_temperature)
 
 void BME280_ReadPressure (double *pressure)
 {
+	HAL_StatusTypeDef checkP;
 	uint8_t pressure_data [3];
 
-	HAL_I2C_Mem_Read (&hi2c2, BME280_ADDRESS << 1, PRESS_MSB_REG, I2C_MEMADD_SIZE_8BIT, pressure_data, 3, 1000);
+	checkP = HAL_I2C_Mem_Read (&hi2c2, BME280_ADDRESS << 1, PRESS_MSB_REG, I2C_MEMADD_SIZE_8BIT, pressure_data, 3, 1000);
+	if (checkP != HAL_OK)
+	{
+		printf ("Error");
+	}
 	*pressure = ((uint32_t) pressure_data [0] << 12 | (uint32_t) pressure_data [1] << 4 | (uint32_t) pressure_data [2] >> 4);
 	*pressure = (*pressure / 256.0); // Convert to Pascal
 }
 
 void BME280_ReadTemperature (double *temperature)
 {
+	HAL_StatusTypeDef checkT;
 	uint8_t temperature_data [3];
 
-	HAL_I2C_Mem_Read(&hi2c2, BME280_ADDRESS << 1, BME280_TEMPERATURE_MSB_REG, I2C_MEMADD_SIZE_8BIT, temperature_data, 3, 1000);
+	checkT = HAL_I2C_Mem_Read(&hi2c2, BME280_ADDRESS << 1, BME280_TEMPERATURE_MSB_REG, I2C_MEMADD_SIZE_8BIT, temperature_data, 3, 1000);
+	if (checkT != HAL_OK)
+	{
+		printf ("Error");
+	}
 	int32_t adc_T = ((uint32_t) temperature_data [0] << 12) | ((uint32_t) temperature_data [1] << 4) | (temperature_data [2] >> 4);
 	int32_t t1, t2, T;
 
